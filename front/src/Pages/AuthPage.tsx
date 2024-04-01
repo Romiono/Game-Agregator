21a111f731343105f73ec7216b96355a1fc36d50
Romiono <roma.serebrennikov.00@mail.ru>
2024-04-02 01:05:48 +0300

    add navbar, authPage

---
+++ front/src/Pages/AuthPage.tsx
@@ -0,0 +1,13 @@
+
+
+const AuthPage = () => {
+
+    return (
+        <div className={'w-full h-full flex justify-center items-center'}>
+            <button onClick={() => {}}>Registration</button>
+            <button onClick={() => {}}>Login</button>
+        </div>
+    )
+};
+
+export default AuthPage;
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,11 +4,27 @@ import App from './App.tsx';
 import './index.css';
 import {Provider} from 'react-redux';
 import {store} from "./store/store.ts";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import AuthPage from "./Pages/AuthPage.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [{
+            element: <AuthPage/>,
+            path: '/login',
+        }]
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
+    </React.StrictMode>,
 );
