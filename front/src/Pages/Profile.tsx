6b4a2a75b4e4f47c9179147d13bc87262f922fbc
Romiono <roma.serebrennikov.00@mail.ru>
2024-04-08 19:47:59 +0300

    bugs fixes

--- front/src/Pages/Profile.tsx
+++ front/src/Pages/Profile.tsx
@@ -3,10 +3,10 @@ import {useAppSelector} from "../hooks/reduxHooks.ts";
 const Profile = () => {
     const {id, username, token} = useAppSelector(state => state.user)
     return (
-        <div className={'container mx-auto'}>
+        <div className={'container mx-auto p-5'}>
             <h3 className={'text-2xl'}>имя: {username}</h3>
             <h5 className={'text-sm'}>id: {id}</h5>
-            <p className={'text-xs'}>токен(временно, для разработки){token}</p>
+            <p className={'text-xs break-all'}>токен(временно, для разработки) <br/> {token}</p>
         </div>
     );
 };
