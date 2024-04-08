292f7133ea56189cc0e038a5859cafc6fc1ee32f
Romiono <roma.serebrennikov.00@mail.ru>
2024-04-08 18:31:19 +0300

    add logout fix mobile menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
 import './index.css'
-import {Link, Outlet} from "react-router-dom";
-import {useAppSelector} from "./hooks/reduxHooks.ts";
+import {Link, Outlet, useNavigate} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "./hooks/reduxHooks.ts";
+import {logout} from "./store/reducers/UserSlice.ts";
 
 function App() {
     const {isAuth} = useAppSelector(state => state.user)
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const userLogOut = () => {
+        dispatch(logout());
+        navigate('/login');
+    }
     return (
         <div className={"w-full h-full flex flex-col"}>
             <div className="navbar bg-base-100">
@@ -17,23 +25,30 @@ function App() {
                                       d="M4 6h16M4 12h8m-8 6h16"/>
                             </svg>
                         </div>
-                        <ul tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li><a>Item 3</a></li>
-                            <li><Link to={'/login'} className="btn">Login</Link></li>
-                        </ul>
+                        {isAuth ?
+                            <ul tabIndex={0}
+                                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 gap-2">
+                                <li><Link to={'/main'} className="font-bold text-xl">Main</Link></li>
+                                <li><Link to={'/profile'} className="font-bold text-xl">Profile</Link></li>
+                                <li>
+                                    <button onClick={userLogOut} className={'btn'}>Logout</button>
+                                </li>
+
+                            </ul> :
+                            <ul tabIndex={0}
+                                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><Link to={'/login'} className="btn">Login</Link></li>
+                            </ul>
+                        }
                     </div>
                     <a className="btn btn-ghost text-xl">daisyUI</a>
                 </div>
                 <div className="navbar-end hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {/*вставлять ссылки*/}
-                    </ul>
                     {isAuth ?
                         <ul className="menu menu-horizontal px-1 flex gap-3">
-                            <Link to={'/main'} className="font-bold text-xl">Main</Link>
-                            <Link to={'/profile'} className="font-bold text-xl">Profile</Link>
+                            <li><Link to={'/main'} className="font-bold text-xl">Main</Link></li>
+                            <li><Link to={'/profile'} className="font-bold text-xl">Profile</Link></li>
+                            <li><button onClick={userLogOut} className={'btn'}>Logout</button></li>
                         </ul>
                         :
                         <Link to={'/login'} className="btn">Login</Link>
